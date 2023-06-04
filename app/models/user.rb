663bbac9cc5e287ed82b4562663ca8efbabb7f5e
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-06-04 06:57:56 +0000

    Add ユーザーログイン・新規会員登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :account_id, length: { in: 6..12 }, format: { with: /\A[0-9a-zA-Z]*\z/ }
+  validates :password, length: { in: 6..12 },  format: { with: /\A[0-9a-zA-Z]*\z/ }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 2023_06_04_030404) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.string "account_id", null: false
+    t.string "introduction"
+    t.integer "profile_image"
+    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
