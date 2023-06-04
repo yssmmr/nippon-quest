a3bdd9023083925e534a0cb76c1f0949bfa1b2b2
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-06-04 08:25:50 +0000

    UPDATE ログイン後にルートパスへ遷移

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
