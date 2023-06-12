c8b542f00cbbc8a40437119e41d50ddd88f29508
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-06-12 21:02:43 +0000

    Add投稿編集機能

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
 
-    if @post.update!(post_params)
+    if @post.update(post_params)
     redirect_to post_path(@post.id)
     else
     render :edit
