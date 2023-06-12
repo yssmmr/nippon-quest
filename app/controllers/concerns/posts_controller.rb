8caa8d02a86d83de5fe1bb916a6fa460a9baf5d8
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-06-12 20:53:46 +0000

    UPDATE投稿編集

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -27,10 +27,19 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+
+    if @post.update!(post_params)
+    redirect_to post_path(@post.id)
+    else
+    render :edit
+
+    end
   end
 
   def destroy
