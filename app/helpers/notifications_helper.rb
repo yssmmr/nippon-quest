451c6c5114e8aec3ccda2a1205f7166f0f0881d9
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-07-22 04:19:42 +0000

    UPDATEレイアウト文字色

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -9,7 +9,7 @@ def notification_form(notification)
 	    when "follow" then
 	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"さんがあなたをフォローしました。"
 	    when "favorite" then
-	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"さんが"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にいいねしました。"
+	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold; color:#FFFFFF;")+"さんが"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold; color:#FFFFFF;")+"にいいねしました。"
 	  end
 	end
 
