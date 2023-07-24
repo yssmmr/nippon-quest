f956e2dbeab08b42155b45e870a1daba8fcd8afa
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-07-24 13:16:16 +0000

    UPDATE

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -7,7 +7,7 @@ def notification_form(notification)
 	  #notification.actionがフォローかいいね
 	  case notification.action
 	    when "follow" then
-	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold;")+"さんがあなたをフォローしました。"
+	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold; color:#FFFFFF;")+"さんがあなたをフォローしました。"
 	    when "favorite" then
 	      tag.a(notification.visitor.name, href:user_path(@visitor), style:"font-weight: bold; color:#FFFFFF;")+"さんが"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold; color:#FFFFFF;")+"にいいねしました。"
 	  end
