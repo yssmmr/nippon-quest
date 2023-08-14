780cce6bb93c6ac1a479c33bbc4d1a66d470d700
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-08-14 13:46:26 +0000

    ADMIN機能作成

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,11 @@
+class Admin::PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,15 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
