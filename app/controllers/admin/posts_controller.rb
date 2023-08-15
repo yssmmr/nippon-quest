c3dc36f0bdb5a1ec1689b9c9bbf061699ee3385d
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-08-15 13:01:03 +0000

    UPDATE　Admin各種機能

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,11 +1,12 @@
 class Admin::PostsController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def index
-
+    @posts = Post.all.page(params[:page])
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def destroy
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,14 @@
 class Admin::UsersController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def index
-
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true).where(is_deleted: false).order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def show
-
+    @user = User.find(params[:id])
+    @posts = @user.posts.all.page(params[:page])
   end
 
   def destroy
