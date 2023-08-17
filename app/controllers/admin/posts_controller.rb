d57b4770d4b02b15b20c5706468f838f7d1a321d
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-08-17 12:12:04 +0000

    UPDATEヘッダー変更

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,4 @@
 class Admin::PostsController < ApplicationController
-  skip_before_action :authenticate_user!
 
   def index
     @posts = Post.all.page(params[:page])
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  skip_before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     admin_posts_path
