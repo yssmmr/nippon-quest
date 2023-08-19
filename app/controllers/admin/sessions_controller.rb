3d1940826fdd1f13d20cb9f3c30296e25f38f5d8
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-08-19 05:22:13 +0000

    UPDATE

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,11 +3,15 @@
 class Admin::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(resource)
+    flash[:alert] = "▽ログインに成功しました！"
     admin_posts_path
+
   end
 
   def after_sign_out_path_for(resource)
+    flash[:alert] = "▽ログアウトに成功しました！"
     new_admin_session_path
+
   end
 
   # before_action :configure_sign_in_params, only: [:create]
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,12 +7,12 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     current_user.create_notification_follow!(current_user, @user)
 
-    redirect_to request.referer, alert: "▽フォローしました！"
+    redirect_to request.referer
   end
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer, alert: "▽フォローを外しました！"
+    redirect_to request.referer
   end
   # フォロー一覧
   def followings
